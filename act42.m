9008c8601bf746656671dbf7e5eaa2a60512dcaa
dagp-ucol <dgentner@ucol.mx>
2022-04-06 10:42:37 -0500

    Actividad 4.2 XS

--- act42.m
+++ act42.m
@@ -1,20 +1,32 @@
 close all 
 clear 
-I= imread("cameraman.tif"); 
+% La imagen se lee.
+I= imread("cameraman.tif");
+% En estas variables se guarda el tamaño de la imagen.
 [r,c,d]=size(I); 
+% Aquí se comprueba que la imagen sea blanco y negro.
 if d>1 
  I=rgb2gray(I); 
-end 
+end
+% La imagen se convierte en tipo de dato double.
 I=im2double(I); 
+% Se aplica la degradación motion con la función fspecial().
 PSF = fspecial('motion', 100, 450); 
-ID = imfilter(I, PSF, 'conv','circular'); 
-MR= 0; 
-VR = 0.0005; 
-IDR = imnoise(ID, 'gaussian',MR,VR); 
-Est_Ruido = VR/var(I(:)); 
+% Se aplica el filtro de convolución circular.
+ID = imfilter(I, PSF, 'conv','circular');
+% En estas variables se especifíca el nivel de ruido.
+MR = 0; % MR = valores de luminosidad.
+VR = 0.001; % VR = Nivel de ruido.
+% Se aplica ruido gaussiano usando las variables anteriores.
+IDR = imnoise(ID, 'gaussian',MR,VR);
+% Se agregan los valores y patrones de la imagen en la variable.
+Est_Ruido = VR/var(I(:));
+% Se utilizan variables anteriores que contienen ruido, degradación e
+% información para restaurar la imagen.
 IrFW = deconvwnr(IDR, PSF, Est_Ruido); 
+% Se muestran los resultados en un subplot.
 figure 
-subplot(2,2,1), imshow(I), title('Imágen Original'); 
-subplot(2,2,2), imshow(ID), title('Imágen I (Conv Circular)');
-subplot(2,2,3), imshow(IDR), title('Imágen IDR (imnoise Gaussian)'); 
-subplot(2,2,4), imshow(IrFW), title('Imágen IrFW (deconvwnr (Ruido))');
+subplot(2,2,1), imshow(I), title('Imagen Original'); 
+subplot(2,2,2), imshow(ID), title('Imagen I (Conv Circular)');
+subplot(2,2,3), imshow(IDR), title('Imagen IDR (imnoise Gaussian)'); 
+subplot(2,2,4), imshow(IrFW), title('Imagen IrFW (deconvwnr (Ruido))');
