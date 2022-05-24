4d09b9636302fa65fbc6fdd2d079b04adf1333b7
dagp-ucol <dgentner@ucol.mx>
2022-05-23 21:53:49 -0500

    6.2 PART 5 - 11

--- actividad-6.2/code.m
+++ actividad-6.2/code.m
@@ -16,43 +16,57 @@ figure
 gray_image = rgb2gray(rgb);
 imshow(gray_image); title('Conversión a Escala de Grises');
 
-% % 5. ???????????????
-% % Qué contienen las variables: centers y radii?
+% % 5. Aqui se guardan algunas variables como centers y radii que
+% representan el centro y propiedades de algunas imagenes de los ciruclos
+% en la imágen, ademas se busca su Polaridad oscura.
 [centers, radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark');
+h = viscircles(centers,radii);
+imshow(h);
+
+% % 6. Aqui ya se marcan con una suposición de imagen los círculos, podemos
+% observar que mayormente solo quedán marcados azules y rojos, y una de
+% color verde. 
+[centers, radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark','Sensitivity',0.9);
+delete(h) % Se borra la variable h que contenía variables de center y radii para la identificación de los circulos.
 imshow(rgb);
 h = viscircles(centers,radii);
 
-% % 6. ???????????????
-% [centers, radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark','Sensitivity',0.9);
-% delete(h) % ???????????????
-% imshow(rgb);
-% h = viscircles(centers,radii);
+% % 7. En esta función ya se puede incluso obervar como el codigo
+% identifica los circulos que estan tapados por otros circulos, esto se basa en que 
+% el programa detecta previamente los valores de los circulos, y hace una aproximación.
+[centers, radii] = imfindcircles(rgb,[20 
+25],'ObjectPolarity','dark','Sensitivity',0.92);
+delete(h); 
+h = viscircles(centers,radii);
 
-% % 7. ???????????????
-% [centers, radii] = imfindcircles(rgb,[20 
-% 25],'ObjectPolarity','dark','Sensitivity',0.92);
-% delete(h); 
-% h = viscircles(centers,radii);
+% % 8. Nuevamente se hace la indentificación de los círculos a excepción
+% de las amarillos que deben ser indentificados posteriormente con otros
+% valores para no confundirse con valores del fondo.
+[centers, radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark','Sensitivity',0.92,'Method','twostage');
+delete(h);
+h = viscircles(centers,radii);
 
-% % 8. ???????????????
-% [centers, radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark','Sensitivity',0.92,'Method','twostage');
-% delete(h);
-% h = viscircles(centers,radii);
+% % 9. Aqui se realiza el uso de la variable PhaseCode en "imfindcircles", lo
+% cual ayuda a determinar la sensibilidad para identifdicar círculos, sin
+% embargo podemos notar que los círculos de color amarillo aun no están.
+[centers, radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark','Sensitivity',0.95,'Method','PhaseCode');
+delete(h);
+viscircles(centers,radii);
 
-% % 9. ???????????????
-% [centers, radii] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark','Sensitivity',0.95,'Method','PhaseCode');
-% delete(h);
-% viscircles(centers,radii);
+% 10. Hay algún tipo de círculos que no se encuentran?
+% Si, en el siguiente código se aplican más filtros de sensibilidad, y
+% solo quedá un círculo a la derecha sin seleccionar ya que muy
+% probablemente MatLab lo confunde con la alta exposición a la luz en la
+% que el objeto se encuentra.
+[centersBright, radiiBright] = imfindcircles(rgb,[20 
+25],'ObjectPolarity','bright','Sensitivity',0.92);
+hBrigth = viscircles(centersBright, radiiBright,'EdgeColor','b');
 
-% % Hay algún tipo de círculos que no se encuentran?
-% % ??????????????????
-% [centersBright, radiiBright] = imfindcircles(rgb,[20 
-% 25],'ObjectPolarity','bright','Sensitivity',0.92);
-% hBrigth=viscircles(centersBright, radiiBright,'EdgeColor','b');
-
-% % ???????
-% delete(hBright)
-% [centersBright, radiiBright, metricBright] = imfindcircles(rgb,[20 25],'ObjectPolarity','bright','Sensitivity',0.92,'EdgeThreshold',0.1);
-% hBright = viscircles(centersBright, radiiBright,'EdgeColor','b');
-% [centersDark, radiiDark, metricDark] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark','Sensitivity',0.92,'EdgeThreshold',0.1);
-% hDark = viscircles(centersDark, radiiDark,'EdgeColor','g');
+% 11. Finalmente con mas filtros  donde se determina el brillo, la oscuridad,
+% el contorno y ademas varias métricas de luminosidad, es posible
+% seleccionar todos los círculos correctamente e incluso se podrían extraer
+% de la imágen.
+[centersBright, radiiBright, metricBright] = imfindcircles(rgb,[20 25],'ObjectPolarity','bright','Sensitivity',0.92,'EdgeThreshold',0.1);
+hBright = viscircles(centersBright, radiiBright,'EdgeColor','b');
+[centersDark, radiiDark, metricDark] = imfindcircles(rgb,[20 25],'ObjectPolarity','dark','Sensitivity',0.92,'EdgeThreshold',0.1);
+hDark = viscircles(centersDark, radiiDark,'EdgeColor','g');
