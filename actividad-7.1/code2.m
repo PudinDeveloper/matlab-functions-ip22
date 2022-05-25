5c1476bf74b191ac3c52be9d5180e4547b0238bc
dagp-ucol <dgentner@ucol.mx>
2022-05-25 10:45:30 -0500

    7.1 optimus prime

---
+++ actividad-7.1/code2.m
@@ -0,0 +1,18 @@
+tronco = imread("./imagenes/tronco.bmp");
+mariposa = imread("./imagenes/mariposa.bmp");
+troncoMariposa = imread("./imagenes/troncoMariposa.bmp");
+
+mariposaF = fft2(mariposa);
+troncoMariposaF = fft2(troncoMariposa);
+
+mConj = conj(mariposaF);
+
+t = mConj .* troncoMariposaF;
+
+it= fftshift(ifft2(t));
+figure, subplot(1,2,1),
+imshow(troncoMariposa);
+subplot(1,2,2),
+imshow(real(it), []);
+
+% mesh(it);
