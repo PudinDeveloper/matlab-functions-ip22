a95c21ddd88b123b60889b489db761f46ae57cfb
dagp-ucol <dgentner@ucol.mx>
2022-05-27 09:38:56 -0500

    7.1 - IMGS

--- actividad-7.1/code.m
+++ actividad-7.1/code.m
@@ -1,12 +1,14 @@
 tronco = imread("./imagenes/tronco.bmp");
-mariposa = imread("./imagenes/mariposa.bmp");
+mariposa = imread("./imagenes/mariposaBlanco.bmp");
 troncoMariposa = imread("./imagenes/troncoMariposa.bmp");
 
 mariposaF = fft2(mariposa);
 troncoMariposaF = fft2(troncoMariposa);
 
+% Defase
 mConj = conj(mariposaF)./abs(mariposaF);
 
+% punto elevado
 t = mConj .* troncoMariposaF;
 
 it= fftshift(ifft2(t));
@@ -16,3 +18,4 @@ subplot(1,2,2),
 imshow(real(it), []);
 
 % mesh(it);
+%Fondo a blanco del objetivo
